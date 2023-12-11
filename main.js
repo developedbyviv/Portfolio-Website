4edcbc37aa4f3780d94e158375f371da70c7e8eb
developedbyviv <developedbyviv@gmail.com>
2023-12-12 01:20:31 +0530

    initialsing home,about and service

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new typed(".typed", {
+  Strings: ["Tech Enthuastic", "Full Stack Web Developer", "Database Adminstrator"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
